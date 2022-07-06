0c0c6f535313e11b78dca6957a1c524d1e72cf36
Stephen <stephen@gilmore.us>
2022-07-06 13:36:10 -0700

    finished ch 13 (p99)

--- homepage/views.py
+++ homepage/views.py
@@ -8,4 +8,8 @@ class HomepageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["my_statement"] = "Nice to see  you!"
+        context["math_result"] = self._do_math()
         return context
+
+    def _do_math(sefl):
+        return 6 + 6
